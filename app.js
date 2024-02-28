4994b24b635290f07d2c02e687e2362fa703e469
mercy-5120 <mgashau18@gmail.com>
2024-02-28 12:56:19 +0300

    Did a small project

---
+++ app.js
@@ -0,0 +1,21 @@
+/*function People(name,gender,occupation,maritalstatus,yob){
+    this.name=name
+    this.gender=gender
+    this.occupation=occupation
+    this.maritalstatus=maritalstatus
+    this.yob=yob
+     
+    this.calage=function(){
+let age=new Date().getFullYear() - this.yob
+console.log(`You are currently ${age} years old`)
+}
+}
+let person1=new People("Mercy","Female","Manager","Single",2004)
+let person2=new People("Esther","Female","Auditor","Married",1975)
+let person3=new People("Hellen","Female","Nurse","Single",2003)
+let person4=new People("Celine","Female","Doctor","Single",2001)
+let person5=new People("Vivian","Female","CEO","Single",1994)
+person1.calage()
+console.log(person1)*/
+
+
